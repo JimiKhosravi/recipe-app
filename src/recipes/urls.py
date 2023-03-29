f16b2fbe77d58852c0ba64c776c170320f38c3e2
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-03-29 13:48:10 +0200

    User authentication in Django

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, RecipeListView, RecipeDetailView
+from .views import home, RecipeListView, RecipeDetailView, records
 
 app_name = 'recipes'
 
 
 urlpatterns = [
-    path('', home),
+    path('home/', home, name='home'),
     path('recipes/list/', RecipeListView.as_view(), name='list'),
-    path('recipes/list/<pk>', RecipeDetailView.as_view(), name='detail')
+    path('recipes/list/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path('recipes/', records, name='records')
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render  # imported by default
 # to display lists and details
 from django.views.generic import ListView, DetailView
 from .models import Recipe  # to access Recipe model
+# to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
+# to protect function-based views
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -9,14 +14,22 @@ from .models import Recipe  # to access Recipe model
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
-# Create your views here.
 
-
-class RecipeListView(ListView):  # class-based view
+class RecipeListView(LoginRequiredMixin, ListView):  # class-based view
     model = Recipe  # specify model
     template_name = 'recipes/main.html'  # specify template
 
 
-class RecipeDetailView(DetailView):  # class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):  # class-based view
     model = Recipe  # specify model
     template_name = 'recipes/detail.html'  # specify template
+
+# define function-based view - records(records()
+
+# keep protected
+
+
+@login_required
+def records(request):
+    # do nothing, simply display page
+    return render(request, 'recipes/records.html')
